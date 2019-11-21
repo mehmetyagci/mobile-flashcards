f1b9b8faa84a7ad7b1c908760a6c72c7ff9be388
Mehmet Yağcı <mehmetyagci53@gmail.com>
2019-11-21 14:33:04 +0300

    3.1 - Update AddDeck,Deck,DeckList and helpers

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { getDeckMetaInfo } from "..utils/helpers";
 
 export default class DeckList extends Component {
-  render () {
+  render() {
     return (
       <View>
         <Text>Deck List</Text>
+        {JSON.stringify(getDeckMetaInfo())}
       </View>
     );
   }
